e5ad78d18a367bd1c2e423d907bff064b172f9cc
arizzuto <aaron.rizzuto23@gmail.com>
2022-08-04 16:16:16 +1000

    testing some strange behaviour;

--- src/action.ts
+++ src/action.ts
@@ -10,9 +10,9 @@ async function codeArtifactGithubAction(): Promise<void> {
     const repository = getInput('repository')
     const scope = getInput('scope')
     const packageTypeRaw = getInput('packageType')
-    console.log(packageTypeRaw)
-    console.log(typeof packageTypeRaw)
     console.log('_____!!!_____')
+    console.log(packageTypeRaw)
+    //console.log(typeof packageTypeRaw)
     console.log((packageTypeRaw == 'npm'))
     const packageType = parsePackageType(packageTypeRaw)
 
