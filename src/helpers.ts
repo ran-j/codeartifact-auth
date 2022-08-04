9d0324ba6a5eda4e4e3e7919c68c786702f5e11e
arizzuto <aaron.rizzuto23@gmail.com>
2022-08-04 16:06:58 +1000

    adding more prints

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,18 @@
 import {packageTypes} from './types'
 
 export function parsePackageType(input: string): packageTypes {
-  if (input == 'npm')
+  console.log('___!!!!!____')
+  console.log(input)
+  console.log(typeof input)
+  const comparitor = 'npm'
+  console.log((comparitor == input))
+  console.log(typeof comparitor)
+
+  if (input == comparitor)
     return packageTypes.npm
   if (input == 'poetry')
     return packageTypes.poetry
 
-  throw new Error(`invalid package type: ${input.trim()}, supported types: ${JSON.stringify(packageTypes)}`)
+  throw new Error(`invalid package type: ${input}, supported types: ${JSON.stringify(packageTypes)}`)
 }
 
